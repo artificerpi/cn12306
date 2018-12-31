20af85b6604dbe30846bfe10bc1a920c8444f59e
artificerpi <artificerpi@outlook.com>
2019-01-01 00:15:22 +0800

    update doc of cache

--- cache.go
+++ cache.go
@@ -2,27 +2,33 @@ package main
 
 import "sync"
 
+// Data is the value type of cache.
 type Data interface{}
 
+// The Cache interface represents an entity you can load and store data.
 type Cache interface {
 	Load(key string) (*Data, bool)
 	Store(key string, d *Data)
 }
 
+// SimpleCache is a simple implementation of Cache with a read-write lock.SimpleCache
+// Note that rw-lock is locked on the methods rather than bucket of map
 type SimpleCache struct {
-	lock sync.RWMutex
+	sync.RWMutex
 	data map[string]Data
 }
 
+// Load loads data from cache by key
 func (c *SimpleCache) Load(key string) (*Data, bool) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
+	c.RLock()
+	defer c.RUnlock()
 	d, ok := c.data[key]
 	return &d, ok
 }
 
+// Store saves data to cache with specified key
 func (c *SimpleCache) Store(key string, d *Data) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
+	c.Lock()
+	defer c.Unlock()
 	c.data[key] = *d
 }
